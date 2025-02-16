1bddeea694d9ba4f258906bac4d5b0efd72d56d8
XandersonSilva <isacsilvasouza2@gmail.com>
2025-02-16 12:36:59 -0300

    java e terminal

---
+++ java/Exercicios/sintaxe basica java/src/edu/xanderson/terminal_e_argumentos/AboutMe.java
@@ -0,0 +1,42 @@
+package edu.xanderson.terminal_e_argumentos;
+import java.util.Scanner;
+import java.util.Locale;//Importante devido a entrada de dados décimais
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+
+        // Preenchedo dados via argumentos na execução via terminal
+        /* 
+         * String nome = args[0];
+         * String sobrenome = args[1];
+         * int idade =   Integer.valueOf(args[2]);
+         * double altura = Double.valueOf(args[3]);
+         * 
+         * System.out.println("Olá, me chamo "+ nome +" "+sobrenome);
+         * System.out.println("Tenho " +idade+" anos");
+         * System.out.println("Minha altura é " + altura + " m");
+        */
+
+
+        // Pegando dados via Scanner 
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe seu nome:");
+        String nome = input.nextLine();
+        System.out.println("Informe seu sobrenome:");
+        String sobrenome = input.nextLine();
+        System.out.println("Informe sua idade:");
+        int idade = input.nextInt();
+        System.out.println("Informe sua altura (em metros):");
+        double altura = input.nextDouble();
+
+        input.close();
+        
+
+        System.out.println("Olá, me chamo "+ nome +" "+sobrenome);
+        System.out.println("Tenho " +idade+" anos");
+        System.out.println("Minha altura é " + altura + " m");
+
+    }
+}
---
+++ java/Exercicios/sintaxe basica java/src/edu/xanderson/terminal_e_argumentos/MinhaClasse.java
@@ -0,0 +1,10 @@
+package edu.xanderson.terminal_e_argumentos;
+
+public class MinhaClasse {
+
+public static void main(String[] args) {
+    System.out.println("Olá, fui executado pelo terminal!");
+}
+
+
+}
