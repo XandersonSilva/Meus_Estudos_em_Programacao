1e0a5b4684f218e5a2325fec832fdb55e615e11a
XandersonSilva <isacsilvasouza2@gmail.com>
2025-04-29 14:23:48 -0300

    Iniciando o desafio sudoku com java

---
+++ java/Exercicios/Sudoku/Sudoku/src/entidades/Quadrado.java
@@ -0,0 +1,34 @@
+package entidades;
+
+public class Quadrado {
+    private int     numero;
+    public  int     atual;
+    private boolean fixo       = false;
+    private boolean preenchido = false;
+
+    public boolean verificaCorreto(){
+        //Retorna se o número que o usuário informou está correto
+        return true ? this.atual == this.numero : false;
+    }
+
+    public void informarNumero(int numero, boolean fixo){
+        if(this.fixo)return;
+
+        this.numero     = numero;
+        this.atual      = numero;
+        this.preenchido = true;
+        this.fixo       = fixo;
+    }
+
+    public void preencerNumero(int numero){
+        if(this.fixo)return;
+        
+        this.numero = numero;
+        this.preenchido = true;
+    };
+
+    public boolean estaPreencido(){
+        return this.preenchido;
+    }
+
+}
