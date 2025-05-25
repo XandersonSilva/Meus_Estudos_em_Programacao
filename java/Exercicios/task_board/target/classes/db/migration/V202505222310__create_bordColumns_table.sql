62cc9cfc027b110911bd2b5e000db60009fa0d5c
XandersonSilva <isacsilvasouza2@gmail.com>
2025-05-25 18:23:50 -0300

    projeto movido

--- java/Exercicios/task_board/target/classes/db/migration/V202505222310__create_bordColumns_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE board_columns(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    board_id BIGINT NOT NULL,
-    title VARCHAR(50),
-    kind VARCHAR(50) NOT NULL,
-    _order INT NOT NULL,
-    CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE,
-    CONSTRAINT uk_board_id_order UNIQUE KEY unique_board_id_order (board_id, _order)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
