fd1036e2b3d1dde6d93d36c53ac8d414f5e484a5
XandersonSilva <isacsilvasouza2@gmail.com>
2025-05-24 23:35:17 -0300

    Adicionando funcionalidades ao cardDAO

--- java/Exercicios/task_board/src/main/java/xanderson/edu/taskBoard/persistence/CardDAO.java
+++ java/Exercicios/task_board/src/main/java/xanderson/edu/taskBoard/persistence/CardDAO.java
@@ -24,4 +24,53 @@ public class CardDAO {
         }
     }
     
+    public void updateColumn(long id, long column_id){
+        String sql = "UPDATE cards c set column_id = ? WHERE c.id = ?;";
+        try (
+            Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(sql);
+        ){
+            prepareStatement.setLong(1, column_id);
+            prepareStatement.setLong(2, id);
+
+            prepareStatement.execute();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateBlockCard(long id, String blockin_reason){
+        String sql = "UPDATE cards c set blocked = ? , blockin_reason = ? WHERE c.id = ?;";
+        try (
+            Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(sql);
+        ){
+            prepareStatement.setBoolean(1, true);
+            prepareStatement.setString(2, blockin_reason);
+            prepareStatement.setLong(3, id);
+
+            prepareStatement.execute();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void updateUnblockCard(long id, String unblockin_reason){
+        String sql = "UPDATE cards c set blocked = ?, blockin_reason = ? WHERE c.id = ?;";
+        try (
+            Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement prepareStatement = connection.prepareStatement(sql);
+        ){
+            prepareStatement.setBoolean(1, true);
+            prepareStatement.setString(2, unblockin_reason);
+            prepareStatement.setLong(3, id);
+
+            prepareStatement.execute();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
